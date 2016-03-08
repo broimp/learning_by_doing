e36b2aa52670e10625985b0f4ae06684fc185fb9
Brooks Imperial <brooks@forethought.net>
2016-03-08 15:08:31 -0700

    Refactored [#114844935]

--- methods_and_recursion/Readme.md
+++ methods_and_recursion/Readme.md
@@ -1,12 +1,11 @@
-#### bottles_of_beer guard results
+#### bottles_of_beer guard results - after refactoring
 
 ```
-rookss-MBP:learning_by_doing $ guard
-13:18:01 - INFO - Inspecting Ruby code style of all files
+[1] guard(main)> all
+15:01:03 - INFO - Run all
+15:01:03 - INFO - Inspecting Ruby code style of all files
 Inspecting 7 files
 .......
 
 7 files inspected, no offenses detected
-13:18:01 - INFO - Guard is now watching at '/Users/bi/workspace/davinci_coders_t1_2016/homework/learning_by_doing'
-[1] guard(main)>
 ```
--- methods_and_recursion/bottles_of_beer.rb
+++ methods_and_recursion/bottles_of_beer.rb
@@ -2,43 +2,37 @@
 
 # bottles of beer recursively
 class Lyrics
-  def initialize(bots = 99)
-    @bots = bots
+  def initialize
+    @bots = 99
+    @bob = 'bottle of beer'
+    @bos = 'bottles of beer'
+    stanzas
   end
 
   def stanzas
-    return if @bots == 1
-    print "#{@bots} bottles of beer on the wall, "
-    print "#{@bots} bottles of beer."
-    puts
+    if @bots == 1
+      puts "#{@bots} #{@bob} on the wall, #{@bots} #{@bob}."
+      puts "Take one down and pass it around, no more #{@bos} on the wall."
+      puts
+      puts "No more #{@bos} on the wall, no more #{@bos}."
+      print "Go to the store and buy some more, 99 #{@bos} on the wall."
+      return
+    end
     second_stanza_line
-    @bots -= 1
     stanzas
   end
 
   def second_stanza_line
+    puts "#{@bots} #{@bos} on the wall, #{@bots} #{@bos}."
     print "Take one down and pass it around, #{@bots - 1} "
     if @bots == 2
-      puts 'bottle of beer on the wall.'
+      puts "#{@bob} on the wall."
     else
-      puts 'bottles of beer on the wall.'
+      puts "#{@bos} on the wall."
     end
     puts
-  end
-
-  def last_stanzas
-    print @bots
-    puts " bottle of beer on the wall, #{@bots} bottle of beer."
-    print 'Take one down and pass it around, '
-    puts 'no more bottles of beer on the wall.'
-    puts
-    print 'No more bottles of beer on the wall, '
-    puts 'no more bottles of beer.'
-    print 'Go to the store and buy some more, '
-    puts '99 bottles of beer on the wall.'
+    @bots -= 1
   end
 end
 
-song = Lyrics.new
-song.stanzas
-song.last_stanzas
+Lyrics.new
