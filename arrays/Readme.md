2d1306b91a5fb6ca24be5eeff803d84257e0bc5c
Brooks Imperial <brooks@forethought.net>
2016-03-09 10:26:34 -0700

    Made many prescribed changes to clean up code and added Readme for guard output [#115101315]

---
+++ arrays/Readme.md
@@ -0,0 +1,11 @@
+#### array_practice.rb
+
+```
+10:22:26 - INFO - Guard is now watching at '/Users/bi/workspace/davinci_coders_t1_2016/homework/learning_by_doing'
+10:23:45 - INFO - Inspecting Ruby code style: arrays/array_practice.rb
+Inspecting 1 file
+.
+
+1 file inspected, no offenses detected
+[1] guard(main)>
+```
--- arrays/array_practice.rb
+++ arrays/array_practice.rb
@@ -1,15 +1,8 @@
 #!/usr/bin/env ruby
 
-array = []
-
 # "1...2...3...4...5...6...7...8...9...10..."
-(0..9).each do |index|
-  value = index + 1
-  array[index.to_i] = value.to_s
-  print value
-  print '...'
-end
-puts
+array = (1..10).to_a
+puts "#{array.join('...')}..."
 
 # "T-10, 9, 8, 7, 6, 5, 4, 3, 2, 1...  BLASTOFF!"
 print 'T-'
@@ -35,27 +28,29 @@ print 'The second from last element is '
 puts array[array.count - 2]
 
 # "The first four elements are '1, 2, 3, 4'"
-print "The first four elements are '"
-puts "#{array[0..3].to_s.delete('"').delete('[').delete(']')}'"
+print 'The first four elements are '
+puts "'#{array[0..3].join(', ')}'"
 
 # "If we delete 5, 6 and 7 from the array, we're left with
 # [1,2,3,4,8,9,10]"
+array.delete_if { |element| element > 4 && element < 8 }
 print "If we delete 5, 6 and 7 from the array, we're left with "
-puts array.values_at(0..3, 7..9).to_s.delete('"').delete(' ')
+puts array.to_s.delete('"').delete(' ')
 
 # "If we add 5 at the beginning of the array, we're left with
 # [5,1,2,3,4,8,9,10]"
 print "If we add 5 at the beginning of the array, we're left with "
-puts array.values_at(0..3, 7..9).unshift(5).to_s.delete('"').delete(' ')
+puts array.unshift(5).to_s.delete('"').delete(' ')
 
 # "If we add 6 at the end of the array, we're left with
 # [5,1,2,3,4,8,9,10,6]"
 print "If we add 6 at the end of the array, we're left with "
-puts array.values_at(0..3, 7..9).unshift(5).push(6).to_s.delete('"').delete(' ')
+puts array.push(6).to_s.delete('"').delete(' ')
 
 # "Only the elements [9, 10] are > 8."
+array.select! { |element| element > 8 }
 print 'Only the elements '
-puts "#{array.values_at(8, 9).to_s.delete('"')} are > 8."
+puts "#{array.to_s.delete('"')} are > 8."
 
 # "If we remove all the elements, then the length of the array is 0"
 print 'If we remove all the elements, then the length of the array'
