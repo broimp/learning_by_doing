aff30052327bf728ae80ebfddef79aec4c529de5
Brooks Imperial <brooks@forethought.net>
2016-04-07 11:10:41 -0600

    Added context bracketing to spec tests [#115550335]

--- objects_tdd/spec/lib/cashregister_spec.rb
+++ objects_tdd/spec/lib/cashregister_spec.rb
@@ -14,44 +14,56 @@ describe '#total' do
   end
 end
 describe '#purchase(amount)' do
-  it 'should add the purchase amount to the total' do
-    subject = CashRegister.new
-    subject.purchase(3.78)
-    test_string = '$3.78'
-    expect(subject.total).to eq(test_string)
+  context 'effect of purchase on total method' do
+    it 'should add the purchase amount to the total' do
+      subject = CashRegister.new
+      subject.purchase(3.78)
+      test_string = '$3.78'
+      expect(subject.total).to eq(test_string)
+    end
   end
-  it 'should output a string with the purchase and the total' do
-    subject = CashRegister.new
-    test_string = 'purchase($3.78) # => $3.78'
-    expect(subject.purchase(3.78)).to eq(test_string)
+  context 'effect of purchase on output' do
+    it 'should output a string with the purchase and the total' do
+      subject = CashRegister.new
+      test_string = 'purchase($3.78) # => $3.78'
+      expect(subject.purchase(3.78)).to eq(test_string)
+    end
   end
 end
 describe '#pay(amount)' do
-  it 'should subtract the pay amount from the total' do
-    subject = CashRegister.new
-    subject.purchase(5)
-    subject.pay(4)
-    test_string = '$1.00'
-    expect(subject.total).to eq(test_string)
+  context 'effect of payment on total method' do
+    it 'should subtract the pay amount from the total' do
+      subject = CashRegister.new
+      subject.purchase(5)
+      subject.pay(4)
+      test_string = '$1.00'
+      expect(subject.total).to eq(test_string)
+    end
   end
-  describe 'reconciliation' do
-    it 'should show a total of 0 if paid in full' do
-      subject = CashRegister.new
-      subject.purchase(4)
-      test_string = 'pay($4.00) # => $0.00'
-      expect(subject.pay(4)).to eq(test_string)
+  context 'effect of payment on output' do
+    context 'when paid in full' do
+      it 'should show a total of 0' do
+        subject = CashRegister.new
+        subject.purchase(4)
+        test_string = 'pay($4.00) # => $0.00'
+        expect(subject.pay(4)).to eq(test_string)
+      end
     end
-    it 'should make change when overpaid' do
-      subject = CashRegister.new
-      subject.purchase(4)
-      test_string = 'pay($5.00) # => Your change is $1.00'
-      expect(subject.pay(5)).to eq(test_string)
+    context 'when overpaid' do
+      it 'should make change' do
+        subject = CashRegister.new
+        subject.purchase(4)
+        test_string = 'pay($5.00) # => Your change is $1.00'
+        expect(subject.pay(5)).to eq(test_string)
+      end
     end
-    it 'should show an amount due if pay is insufficient' do
-      subject = CashRegister.new
-      subject.purchase(4)
-      test_string = 'pay($2.00) # => Your new total is $2.00'
-      expect(subject.pay(2)).to eq(test_string)
+    context 'when partial payment is made' do
+      it 'should show an amount due' do
+        subject = CashRegister.new
+        subject.purchase(4)
+        test_string = 'pay($2.00) # => Your new total is $2.00'
+        expect(subject.pay(2)).to eq(test_string)
+      end
     end
   end
 end
